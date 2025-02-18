776c40415924bc3cbc0bbabc1e9231a2b078fc2f
Chabomm <ckqha0305@gmail.com>
2025-02-18 17:58:13 +0900

    Time: 0 ms (100%), Space: 18 MB (21%) - LeetHub

---
+++ leetcode/Easy/0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        simbol = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M":1000}
+        total = 0
+
+        for i in range(len(s)-1) :
+            if simbol[s[i]] < simbol[s[i+1]] :
+                total -= simbol[s[i]]
+            else :
+                total += simbol[s[i]]
+        total += simbol[s[-1]]
+        return total
